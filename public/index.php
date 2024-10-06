4ab8abe7056214dd7a0ee663af1bfbb4ab1bb630
Raquelll77 <zilpa.alvarado@unah.hn>
2024-10-06 13:28:18 -0600

    se agrego el enpoint cita y la logica del paginador

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\CitaController;
 use Controllers\LoginController;
 use MVC\Router;
 
@@ -27,6 +28,10 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 //confirmar cuenta
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
+
+//area privada
+$router->get('/cita', [CitaController::class, 'index']);
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funcion;es del Controlador
 $router->comprobarRutas();
--- views/layout.php
+++ views/layout.php
@@ -17,7 +17,7 @@
         </div>
     </div>
 
-
+    <?= $script ?? '' ?>
 </body>
 
 </html>
